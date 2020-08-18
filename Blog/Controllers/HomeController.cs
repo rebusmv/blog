81306fb8987f13e2ebba72de2644115cca21376c
Michael Vassilyev <rebusmv@gmail.com>
2020-08-18 16:21:41 +0600

    добавлена страница Home/Post

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -9,6 +9,14 @@ namespace Blog.Controllers
 {
     public class HomeController:Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
     }
 }
