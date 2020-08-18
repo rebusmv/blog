d4a1fec248959507782c9bbac70a887c5f0bfd0d
Michael Vassilyev <rebusmv@gmail.com>
2020-08-18 16:15:35 +0600

    Структура для статических файлов

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
 namespace Blog
@@ -15,6 +16,7 @@ namespace Blog
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         
@@ -25,15 +27,22 @@ namespace Blog
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Change world2!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Change world2!");
+            //    });
+            //});
         }
     }
 }
